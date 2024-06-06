19555910fba188f65cc22af72a1637a69f0453c5
Oussama <6284942@bdeb.qc.ca>
2024-06-06 16:04:40 -0400

    AjoutBoucle
    Boucle

---
+++ src/main/java/org/example/Oussama.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Oussama {
+    public static void main(String[] args) {
+         System.out.println("Hello World");
+         System.out.println("Hello World");
+        for (int i = 0; i < 6; i++) {
+            
+        }
+    }
+}
