497ab0513afd6dfdc364435d9e7841fd5535df11
1261362 <1261362@bdeb.qc.ca>
2024-06-06 15:29:46 -0400

    Fichier effectuer

---
+++ src/main/java/org/example/Gab_Projet.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Gab_Projet {
+
+    public static void main(String[] args) {
+        System.out.println("Je suis sur la branche a mick sur le projet de gabriel");
+    }
+}
