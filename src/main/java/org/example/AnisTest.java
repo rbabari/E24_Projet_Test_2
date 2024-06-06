91a5a16de832fe1ded6abaeb10c10a5ad7706aa1
Oussama <6284942@bdeb.qc.ca>
2024-06-06 17:00:13 -0400

    Affichage

--- src/main/java/org/example/AnisTest.java
+++ src/main/java/org/example/AnisTest.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class AnisTest {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello  !");
     }
 
 
